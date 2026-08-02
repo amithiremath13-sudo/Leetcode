a0bfc441f8065216fd896b6e3e6811af363fe532
amithiremath13-sudo <amithiremath13@gmail.com>
2026-08-02 21:48:52 +0530

    Time: 1 ms (97.66%), Space: 47.2 MB (57.27%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int i;
+        int n=nums.length;
+        int[] ans = new int[2*n];
+        for(i=0;i<n;i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+            
+            
+        }
+        return ans;
+    }
+}
